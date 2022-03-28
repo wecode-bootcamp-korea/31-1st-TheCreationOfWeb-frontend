357866db0388b31185e750616ea77d0a36b572af
sunnyfterrain <sunnyfterrain@gmail.com>
2022-03-28 17:43:05 +0900

    ADD: CRA 초기세팅 재 수정

---
+++ src/Router.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from 'pages/Main/Main';
+import Login from './pages/Login/Login';
+import Join from './pages/Join/Join';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/member/login" element={<Login />} />
+        <Route path="/member/join" element={<Join />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      Main
+      <div className="aa">d푸룯츠</div>
+    </div>
+  );
+};
+
+export default Main;
