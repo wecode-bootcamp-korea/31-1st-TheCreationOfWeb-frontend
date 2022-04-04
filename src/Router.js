29a05d4a4ee11ba7de0e45b469ebbd2f4b272fc5
노 <shduddhks1234@naver.com>
2022-04-04 21:28:03 +0900

    Aside Header 확인

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from 'pages/Main/Main';
+import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import Join from './pages/Join/Join';
+import TeamMember from './pages/TeamMember/TeamMember';
+import Header from './components/Header/Header';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/member/login" element={<Login />} />
         <Route path="/member/join" element={<Join />} />
+        <Route path="/member/teamMember" element={<TeamMember />} />
       </Routes>
     </BrowserRouter>
   );
