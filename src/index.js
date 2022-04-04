43800176f8ecb9e8c9a829fcf6155e2ce9c98360
sunnyfterrain <sunnyfterrain@gmail.com>
2022-04-04 12:23:35 +0900

    ADD: index.js 내 context 적용

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
-import 'styles/reset.scss';
-import 'styles/common.scss';
+import { FruitzProvider } from './FruitContext';
+import './styles/reset.scss';
+import './styles/common.scss';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(
+  <FruitzProvider>
+    <Router />
+  </FruitzProvider>,
+  document.getElementById('root')
+);
