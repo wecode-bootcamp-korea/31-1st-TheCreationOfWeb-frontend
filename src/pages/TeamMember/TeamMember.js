68c1613ac9a0b26be3c4cad3551c55756873e444
노 <shduddhks1234@naver.com>
2022-04-06 09:52:03 +0900

    test

--- src/pages/TeamMember/TeamMember.js
+++ src/pages/TeamMember/TeamMember.js
@@ -2,12 +2,6 @@ import React from 'react';
 import TeamMemberList from './TeamMemberList';
 import TeamMember_List_Data from './TeamMemberListData';
 const TeamMember = () => {
-  // let [modalList, setModalList] = useState(false);
-
-  // const toggleModalListHandler = () => {
-  //   modalList ? setModalList(false) : setModalList(true);
-  // };
-
   return (
     <div className="teamMember">
       {TeamMember_List_Data.map(teammeber => {
